51e976fdf9bd3eadddf25997a771397ff01aee0c
BrendanGlancy <bglance68@gmail.com>
2021-03-23 18:37:54 -0400

    new challenge

---
+++ Python/stringValidators.py
@@ -0,0 +1,12 @@
+def main():
+    s = input()
+    print(any(map(str.isalnum, s)))
+    print(any(map(str.isalpha, s)))
+    print(any(map(str.isdigit, s)))
+    print(any(map(str.islower, s)))
+    print(any(map(str.isupper, s)))
+    
+    
+if __name__ == '__main__':
+    main()
+    
